7c10bc7811373e88c6c60db04239b4e7e0b6656b
Ubuntu <ubuntu@ip-10-151-126-207.ec2.internal>
2013-07-14 08:20:38 +0000

    Added web.js

--- web.js
+++ web.js
@@ -1,12 +1,18 @@
+var fs = require('fs');
 var express = require('express');
+var app = express();
 
-var app = express.createServer(express.logger());
-
-app.get('/', function(request, response) {
-  response.send('Hello World2!');
+var htmlTable = '';
+fs.readFile('/index.html', function (err, data) {
+  if (err) {
+    throw err;
+  }
+  var parsedData = parseData(data);
+  htmlTable = createTable(parsedData);
 });
 
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
+app.get('/htmlTable', function(req, res){
+  res.send(htmlTable);
 });
+
+app.listen(3000);
