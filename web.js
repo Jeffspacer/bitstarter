671763f7e5eec1a822974d85ed77acaf4834978a
Ubuntu <ubuntu@ip-10-151-126-207.ec2.internal>
2013-07-14 19:59:46 +0000

    Added web.js

--- web.js
+++ web.js
@@ -1,22 +1,10 @@
 var express = require('express');
-
 var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
-
+app.get('/', function(reqquest, response) {
 var fs = require('fs');
-fs.open('readFileSync("index.html")', 'r', function(err, fd) {
-  if (err) { throw err }
-  var readBuffer   = new Buffer(1024),
-      bufferOffset = 0,
-      bufferLength = readBuffer.length,
-      filePosition = 100;
-  fs.read(fd, readBuffer,bufferOffset, bufferLength, filePosition, 
-    function(err, readBytes) {
-      if (err) { throw err; }
-      console.log('just read ' + readBytes + ' bytes');
-      if (readBytes > 0) {
-        console.log(readBuffer.slice(0, readBytes));
-      }      
-    }); 
-});
+var content = fs.readFileSync('index.html', 'utf8');});
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+console.log("Listening on " + port);});
